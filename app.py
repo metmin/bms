abc4f57e3a3ab816ec57ee180a2aece276b394f1
Mehmet Emin Bora <mehmet.bora99@hotmail.com>
2022-05-01 13:39:56 +0300

    devamke

--- app.py
+++ app.py
@@ -4,9 +4,15 @@ import serial
 ser = serial.Serial('/dev/ttyUSB0', 9600, timeout=1)
 ser.reset_input_buffer()
 
-ser.write('cv'.encode('utf-8'))
 
 while True:
-    if ser.in_waiting > 0:
-        line = ser.readline().decode('utf-8').rstrip()
-        print(line)
+
+    print('data gonderiliyor...')
+    ser.write('c'.encode('utf-8'))
+    print('data gonderildi.')
+   
+    while True:
+        if ser.in_waiting > 0:
+            line = ser.readline().decode('utf-8').rstrip()
+            print(line)
+            break
